0242dc7c1075e63089ab6351624dd8bef9ab2796
yuhao8384 <1278599363@qq.com>
2016-06-16 09:11:06 +0800

    Revert "Modern telegram"

--- firmware_mpgl1/application/main.c
+++ firmware_mpgl1/application/main.c
@@ -67,8 +67,8 @@ void main(void)
   SdCardInitialize();
 
   /* Application initialization */
-  UserAppInitialize();
-  UserApp2Initialize();
+    UserAppInitialize();
+  
   /* Exit initialization */
   SystemStatusReport();
   G_u32SystemFlags &= ~_SYSTEM_INITIALIZING;
@@ -92,7 +92,7 @@ void main(void)
 
     /* Applications */
     UserAppRunActiveState();
-    UserApp2RunActiveState();
+    
     /* System sleep*/
     HEARTBEAT_OFF();
     SystemSleep();
